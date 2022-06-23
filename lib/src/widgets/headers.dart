7aea5c3cc24c1dbfd290e94824ae2a09f06f72a4
samuelmh96 <samu.96.h@gmail.com>
2022-06-22 20:21:20 -0400

    0.1

---
+++ lib/src/widgets/headers.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HeaderCuadrado extends StatelessWidget {
+  const HeaderCuadrado({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      color: const Color(0xff008080),
+    );
+  }
+}
