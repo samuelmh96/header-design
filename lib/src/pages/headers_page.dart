a11f5b85ba5846a9f0929dacc755e8bbcc259bbb
samuelmh96 <samu.96.h@gmail.com>
2022-06-23 21:55:26 -0400

    cabecera diagonal con custompainter

--- lib/src/pages/headers_page.dart
+++ lib/src/pages/headers_page.dart
@@ -6,6 +6,6 @@ class HeadersPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: HeaderBordeRedondeado());
+    return const Scaffold(body: HeaderDiagonal());
   }
 }
--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -27,3 +27,47 @@ class HeaderBordeRedondeado extends StatelessWidget {
     );
   }
 }
+
+class HeaderDiagonal extends StatelessWidget {
+  const HeaderDiagonal({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: double.infinity,
+      width: double.infinity,
+      // color: Color(0xff008080),
+      child: CustomPaint(
+        painter: _HeaderDiagonalPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderDiagonalPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final lapiz = Paint();
+
+    //propiedades
+    lapiz.color = const Color(0xff008080);
+    lapiz.style = PaintingStyle.fill;
+    lapiz.strokeWidth = 5;
+
+    // Dibujar con el path y el lapiz
+    final path = Path();
+
+    path.moveTo(0, size.height * 0.30);
+    path.lineTo(size.width, size.height * 0.40);
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+    path.lineTo(0, size.height * 0.5);
+
+    canvas.drawPath(path, lapiz);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
